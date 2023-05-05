e49bd4402321b5315943f81f4f1911b85e7de6e4
renuka gomte <renugomte27@gmail.com>
2023-05-05 19:18:42 +0530

    update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static("public"))
 app.use(cors({
+    // origin:"http://localhost:5173",
     origin: "https://my-portfolio-production-d0a6.up.railway.app/",
     credentials: true
 }))
